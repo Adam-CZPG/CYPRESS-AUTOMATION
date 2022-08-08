a5a4e85ee3471045625216a4d9d0173738ae4349
Akhmadjon Ortikov (Adam) <105177864+Adam-CZPG@users.noreply.github.com>
2022-08-08 03:03:24 +0200

    Github

--- cypress/e2e/QA/Test_2.cy.js
+++ cypress/e2e/QA/Test_2.cy.js
@@ -1,9 +1,12 @@
 describe('czv', () => {
     it('By',() => {
-        cy.visit('https://rozetka.com.ua/');
+        cy.visit('https://www.alza.cz/EN/');
         cy.viewport(1000, 660);
-        cy.get('.basket-2-nd-step-basket desktop').click()
+        cy.get('input[id="edtSearch"]').type('iphone 12');
+        cy.contains('Search').click();
+        cy.contains('iPhone 12 128GB Black').click(); 
+        cy.get('a[class="btnx new green buy js-buy-button"]').click();
    
-    })
+    });
         
 })
